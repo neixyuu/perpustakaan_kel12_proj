f2748383fc17012b6505bc4d0c84821a571b3404
neixyuu <131132775+neixyuu@users.noreply.github.com>
2026-05-23 13:39:07 +0700

    fixing bug not loading, detail book, detail data, location fix

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -26,6 +26,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -55,4 +56,8 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
